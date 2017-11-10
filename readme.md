97ef1267894f8b12536bf92d2c65b5e06c65ae72
Evan Lovely <Evan@eBook.local>
2017-11-10 15:17:17 -0800

    Improving setup by adding ability to use recursive paths. Can also set config in json or yaml.

--- readme.md
+++ readme.md
@@ -25,6 +25,7 @@ plugins:
       - ../root2
     namespaces:
       foo:
+        recursive: true
         paths:
           - ../bar
           - ../baz
@@ -43,6 +44,8 @@ Paths are relative to `composer.json` and `vendor/` in the same directory as the
     - sandy.twig
 - bar/
     - item1.twig
+    - subdir/
+        - subitem1.twig
 - baz/
     - item2.twig
 
@@ -52,10 +55,13 @@ You could now use this in Twig:
 {% include "@mountains/hood.twig" %}
 {% include "@rivers/sandy.twig" %}
 {@ include "@foo/item1.twig" %}
+{@ include "@foo/subitem1.twig" %}
 {@ include "@foo/item2.twig" %}
 ```
 
-You can use either the `roots` or the `namespaces` approach without the other. The `roots` approach is how Pattern Lab registers the namespaces of all folders under `source/_patterns/` like `@atoms` and is therefore useful for including files from other Pattern Labs (watch out for namespace collisions - i.e. you can only have one `@atoms`). And the `namespaces` approach is how the Drupal [Component Libraries](https://www.drupal.org/project/components) module registers Twig namespaces, though it could also be used to register the core modules template files in Pattern Lab so you could `{% extend "@blocks/block.html.twig" %}` if you'd like.
+You can use either the `roots` or the `namespaces` approach without the other. The `roots` approach is how Pattern Lab registers the namespaces of all folders under `source/_patterns/` like `@atoms` and is therefore useful for including files from other Pattern Labs (watch out for namespace collisions - i.e. you can only have one `@atoms`). And the `namespaces` approach is how the Drupal [Component Libraries](https://www.drupal.org/project/components) module registers Twig namespaces, though it could also be used to register the core modules template files in Pattern Lab so you could `{% extend "@blocks/block.html.twig" %}` if you'd like. If using `namespaces` and you set `recursive: true`, then it will add all sub directories. 
+
+Also, you can set `twigNamespaces.readFromFile` to a path to a JSON or Yaml file and it will read those contents and use it in place of `twigNamespaces.namespaces`.
 
 ## Disabling the Plugin
 
--- src/PatternLab/TwigNamespaces/PatternLabListener.php
+++ src/PatternLab/TwigNamespaces/PatternLabListener.php
@@ -15,50 +15,61 @@ namespace PatternLab\TwigNamespaces;
 use \PatternLab\Config;
 use \PatternLab\PatternEngine\Twig\TwigUtil;
 use \Twig_Loader_Filesystem;
+use \BasaltInc\TwigTools;
 
 class PatternLabListener extends \PatternLab\Listener {
-  
+
   /**
-  * Add the listeners for this plug-in
-  */
+   * Add the listeners for this plug-in
+   */
   public function __construct() {
 
     // add listener
     $this->addListener("twigLoaderPreInit.customize","addTwigNamespaces");
-    
+
   }
-  
+
   public function addTwigNamespaces() {
     $config = Config::getOption("plugins.twigNamespaces");
-		$basePath = Config::getOption("baseDir");
-		if ($config["enabled"]) {
+    $basePath = Config::getOption("baseDir");
+    if ($config["enabled"]) {
 
-			// Pattern Lab approach
-			// Each root has each sub-directory added as namespace in the same way that
-			// Pattern Lab's Twig Engine adds main pattern directories as namespaces
-			// `00-atoms` => `@atoms`
-			if (array_key_exists("roots", $config)) {
-				foreach ($config["roots"] as $root) {
-					$loader = new Twig_Loader_Filesystem(array($basePath . $root));
-					TwigUtil::addPaths($loader, $basePath . $root);
-					TwigUtil::addLoader($loader);
-				}
-			}
+      // Pattern Lab approach
+      // Each root has each sub-directory added as namespace in the same way that
+      // Pattern Lab's Twig Engine adds main pattern directories as namespaces
+      // `00-atoms` => `@atoms`
+      if (array_key_exists("roots", $config)) {
+        foreach ($config["roots"] as $root) {
+          $loader = new Twig_Loader_Filesystem(array($basePath . $root));
+          TwigUtil::addPaths($loader, $basePath . $root);
+          TwigUtil::addLoader($loader);
+        }
+      }
 
-			// Drupal approach
-			// Each key becomes the namespace for all `paths` inside
-			// Follows data model from Drupal module: https://www.drupal.org/project/components
-			if (array_key_exists("namespaces", $config)) {
-				$loader2 = new Twig_Loader_Filesystem(array());
-				foreach ($config["namespaces"] as $namespace => $item) {
-					foreach ($item["paths"] as $path) {
-						$loader2->addPath($basePath . $path, $namespace);
-					}
-				}
-				TwigUtil::addLoader($loader2);
-			}
+      $namespacesConfig = [];
+      if (array_key_exists("readFromFile", $config)) {
+        print_r('reading from file...');
+        $namespacesConfig = TwigTools\Utils::getData($config['readFromFile']);
+      } elseif (array_key_exists("namespaces", $config)) {
+        print_r('grabbing from namespaces key in pl config...');
+        $namespacesConfig = $config['namespaces'];
+      }
+      
+      // Drupal approach
+      // Each key becomes the namespace for all `paths` inside
+      // Follows data model from Drupal module: https://www.drupal.org/project/components
+      if ($namespacesConfig) {
+        $loader2 = new Twig_Loader_Filesystem(array());
+        $namespaces = TwigTools\Namespaces::buildLoaderConfig($namespacesConfig, Config::getOption('baseDir'));
+        foreach ($namespaces as $namespace => $item) {
+          foreach ($item["paths"] as $path) {
+            $loader2->addPath($path, $namespace);
+          }
+        }
+        TwigUtil::addLoader($loader2);
+      }
 
-		}
+    }
   }
 
 }
